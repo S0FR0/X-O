91926df6e4237ea0e5f0aaa4b66f52870c0f2d91
Carlos-Marian Sofronea <sofroneacarloss@gmail.com>
2024-05-10 14:57:40 +0300

    create gameBoard and whoMoves fct

--- script.js
+++ script.js
@@ -1,10 +1,40 @@
+let gameBoard = [];
+
+function createBoard() {
+    const table = document.getElementById('table');
+    for (let i = 0; i < 9; i++){
+        const button = document.createElement('button');
+        table.appendChild(button)
+        gameBoard.push(button)
+    }
+}
+
+createBoard();
+
 function player (symbol){
     let moves = 0;
     let smbol = symbol;
     const showMoves = () => moves;
     const addMove = () => moves ++;
-    return {smbol, addMove, showMoves};
+    const removeMoves = () => moves = 0;
+    return {smbol, addMove, showMoves, removeMoves};
 }
 
 const playerOne = player('X');
 const playerTwo = player('O');
+
+function whoMoves(){
+    if (playerOne.showMoves() + playerTwo.showMoves() % 2 === 0 )
+        {
+            console.log('playerOne');
+            return playerOne;
+    }
+    else if (playerOne.showMoves() + playerTwo.showMoves() === 9){
+            console.log('Draw');
+    }
+    else
+        {
+            console.log('playerTwo');
+            return playerTwo;
+        }
+}
