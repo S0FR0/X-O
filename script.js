5c1b7a7f1dda4a5a8d83b48384df2414497aff85
Carlos-Marian Sofronea <sofroneacarloss@gmail.com>
2024-05-10 13:53:02 +0300

    created object factory

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-console.log('Hello world!');
+function player (symbol){
+    let moves = 0;
+    let smbol = symbol;
+    const showMoves = () => moves;
+    const addMove = () => moves ++;
+    return {smbol, addMove, showMoves};
+}
+
+const playerOne = player('X');
+const playerTwo = player('O');
